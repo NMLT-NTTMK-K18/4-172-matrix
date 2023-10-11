80b240166813b6b4ca8e0b74a22f5fcf289a9f99
NTGNguyen <23521049@gm.uit.edu.vn>
2023-10-11 16:17:53 +0700

    Bai003->008.Done

--- Bai003/Source.cpp
+++ Bai003/Source.cpp
@@ -1,8 +1,40 @@
 #include <iostream>
 using namespace std;
 
+void Nhap(int[][500], int);
+void Xuat(int[][500], int);
+
 int main()
 {
-
+	int a[500][500];
+	int n;
+	cin >> n;
+	Nhap(a, n);
+	Xuat(a, n);
 	return 0;
+}
+
+void Nhap(int a[][500], int n)
+{
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < n; j++)
+			cin >> a[i][j];
+}
+
+void Xuat(int a[][500], int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+			cout << a[i][j] << " ";
+		cout << endl;
+	}
+}
+
+void LietKe(int a[][500], int m, int n)
+{
+	for (int i = 0; i < m; i++)
+		for (int j = 0; j < n; j++)
+			if (a[i][j] % 2 == 0)
+				cout << setw(8) << a[i][j];
 }
--- Bai013/Source.cpp
+++ Bai013/Source.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
+void Nhap(int[][500], int);
+void LietKeChan(int[][500], int);
 int main()
 {
-
+	int a[500][500];
+	int n;
+	cin >> n;
+	Nhap(a, n);
+	LietKeChan(a, n);
 	return 0;
+}
+
+void Nhap(int a[][500], int n)
+{
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < n; j++)
+			cin >> a[i][j];
+}
+
+void LietKeChan(int a[][500], int n)
+{
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < n; j++)
+			if (a[i][j] % 2 == 0)
+				cout << setw(8) << a[i][j];
 }
