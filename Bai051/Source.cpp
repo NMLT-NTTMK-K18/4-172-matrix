044e4c54c784160fe5f0a059c41fe0f34e278ee7
KevinNitroG <trannguyenthaibinh46@gmail.com>
2023-10-12 20:04:36 +0700

    💥 Bai(51, 56, 61)

--- Bai051/Source.cpp
+++ Bai051/Source.cpp
@@ -1,8 +1,64 @@
 #include <iostream>
 using namespace std;
 
+void Nhap(float[][500], int &, int &);
+float TongCucTieu(float[][500], int, int);
+int ktCucTieu(float[][500], int, int, int, int);
+
 int main()
 {
+	float a[500][500];
+	int m, n;
+	Nhap(a, m, n);
+
+	cout << "Tong cac phan tu cuc tieu trong ma tran: " << TongCucTieu(a, m, n);
 
 	return 0;
+}
+
+void Nhap(float a[][500], int &m, int &n)
+{
+	cout << "Nhap so dong: ";
+	cin >> m;
+	cout << "Nhap so cot: ";
+	cin >> n;
+	for (int i = 0; i < m; i++)
+		for (int j = 0; j < n; j++)
+		{
+			cout << "Nhap a[" << i << "][" << j << "]: ";
+			cin >> a[i][j];
+		}
+}
+
+float TongCucTieu(float a[][500], int m, int n)
+{
+	float sum = 0;
+	for (int i = 0; i < m; i++)
+		for (int j = 0; j < n; j++)
+			if (ktCucTieu(a, m, n, i, j))
+				sum += a[i][j];
+	return sum;
+}
+
+int ktCucTieu(float a[][500], int m, int n, int i, int j)
+{
+	if (i != 0)
+		if (a[i][j] >= a[i - 1][j])
+			return 0;
+
+	if (i != m - 1)
+		if (a[i][j] >= a[i + 1][j])
+			return 0;
+
+	if (j != 0)
+		if (a[i][j] >= a[i][j - 1])
+			return 0;
+
+	if (j != n - 1)
+		if (a[i][j] >= a[i][j + 1])
+			return 0;
+
+	cout << a[i][j] << endl;
+
+	return 1;
 }
--- Bai056/Source.cpp
+++ Bai056/Source.cpp
@@ -1,8 +1,51 @@
 #include <iostream>
 using namespace std;
 
+void Nhap(int[][500], int &, int &);
+int DemHoanThien(int[][500], int, int);
+int ktHoanThien(int);
+
 int main()
 {
+	int a[500][500];
+	int m, n;
+	Nhap(a, m, n);
 
+	cout << "So luong cac phan tu hoan thien trong ma tran: " << DemHoanThien(a, m, n);
+
+	return 0;
+}
+
+void Nhap(int a[][500], int &m, int &n)
+{
+	cout << "Nhap so dong: ";
+	cin >> m;
+	cout << "Nhap so cot: ";
+	cin >> n;
+	for (int i = 0; i < m; i++)
+		for (int j = 0; j < n; j++)
+		{
+			cout << "Nhap a[" << i << "][" << j << "]: ";
+			cin >> a[i][j];
+		}
+}
+
+int DemHoanThien(int a[][500], int m, int n)
+{
+	int dem = 0;
+	for (int i = 0; i < m; i++)
+		for (int j = 0; j < n; j++)
+			dem += ktHoanThien(a[i][j]);
+	return dem;
+}
+
+int ktHoanThien(int n)
+{
+	int sum = 0;
+	for (int i = 1; i < n; i++)
+		if (n % i == 0)
+			sum += i;
+	if (sum == n)
+		return 1;
 	return 0;
 }
