238d644d053f567d4b398f1c28d6a5265dd374f2
MinhHuy1507 <144987616+MinhHuy1507@users.noreply.github.com>
2023-10-13 18:40:27 +0700

    004 - 019 Done

--- Bai004/Source.cpp
+++ Bai004/Source.cpp
@@ -1,8 +1,41 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
+void Nhap(float[][500], int&, int&);
+void Xuat(float[][500], int, int);
+
 
 int main()
 {
+	float arr[500][500];
+	int m;
+	int n;
 
+	Nhap(arr, m, n);
+	Xuat(arr, m, n);
 	return 0;
+}
+
+void Nhap(float a[][500], int& m, int& n)
+{
+	cout << "Nhap so hang cua ma tran: ";
+	cin >> m;
+	cout << "Nhap so cot cua ma tran: ";
+	cin >> n;
+	for (int i = 0; i < m; i++)
+		for (int j = 0; j < n; j++)
+		{
+			cout << "Nhap a[" << i << "][" << j << "] : ";
+			cin >> a[i][j];
+		}
+}
+
+void Xuat(float a[][500], int m, int n)
+{
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++)
+			cout << setw(8) << setprecision(4) << a[i][j];
+		cout << endl;
+	}
 }
--- Bai009/Source.cpp
+++ Bai009/Source.cpp
@@ -1,8 +1,40 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
+void Nhap(int[][500], int&, int&);
+void Lietke(int[][500], int, int);
+
 
 int main()
 {
+	int arr[500][500];
+	int m;
+	int n;
 
+	Nhap(arr, m, n);
+	Lietke(arr, m, n);
 	return 0;
+}
+
+void Nhap(int a[][500], int& m, int& n)
+{
+	cout << "Nhap so hang cua ma tran: ";
+	cin >> m;
+	cout << "Nhap so cot cua ma tran: ";
+	cin >> n;
+	for (int i = 0; i < m; i++)
+		for (int j = 0; j < n; j++)
+		{
+			cout << "Nhap a[" << i << "][" << j << "] : ";
+			cin >> a[i][j];
+		}
+}
+
+void Lietke(int a[][500], int m, int n)
+{
+	cout << "Cac gia tri chia het cho 2: \n";
+	for (int i = 0; i < m; i++)
+		for (int j = 0; j < n; j++)
+			if (a[i][j] % 2 == 0)
+				cout << setw(8) << a[i][j];
 }
