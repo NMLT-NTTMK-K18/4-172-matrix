959868cf937f9edd52788420589cd2042236be02
Tuệ Dương Thông <23521734@gm.uit.edu.vn>
2023-10-15 15:38:20 +0700

    Bai005. Completed

--- Bai005/Source.cpp
+++ Bai005/Source.cpp
@@ -1,8 +1,40 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
+
+void Nhap(float[][500], int&, int&);
+void Xuat(float[][500], int, int);
 
 int main()
 {
+	float a[500][500];
+	int m, n;
 
+	Nhap(a, m, n);
+	Xuat(a, m, n);
 	return 0;
+}
+
+void Nhap(float a[][500], int& m, int& n)
+{
+	cout << "Nhap so hang: ";
+	cin >> m;
+	cout << "Nhap so cot: ";
+	cin >> n;
+	for (int i = 0; i < m; i++)
+		for (int j = 0; j < n; j++)
+		{
+			cout << "Nhap a[" << i << "][" << j << "]: ";
+			cin >> a[i][j];
+		}
+}
+
+void Xuat(float a[][500], int m, int n)
+{
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++)
+			cout << fixed << setprecision(2) << a[i][j] << setw(10);
+		cout << endl;
+	}
 }
