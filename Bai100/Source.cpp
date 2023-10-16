7a2023d94297c784d003d034004ddc73873bb2ad
Tuệ Dương Thông <23521734@gm.uit.edu.vn>
2023-10-16 12:44:57 +0700

    Bai100. Completed

--- Bai100/Source.cpp
+++ Bai100/Source.cpp
@@ -1,8 +1,70 @@
 #include <iostream>
 using namespace std;
 
+void Nhap(int[][500], int&, int&);
+int ViTriDuongDau(int[], int);
+int ViTriDuongNhoNhat(int[], int);
+int TimChuSo(int[][500], int, int);
+
 int main()
 {
+	int a[500][500];
+	int m, n;
 
+	Nhap(a, m, n);
+	int tcs = TimChuSo(a, m, n);
+	cout << tcs;
 	return 0;
+}
+
+void Nhap(int a[][500], int& m, int& n)
+{
+	cout << "Nhap so hang: ";
+	cin >> m;
+	cout << "Nhap so cot: ";
+	cin >> n;
+	for (int i = 0; i < m; i++)
+		for (int j = 0; j < n; j++)
+		{
+			cout << "Nhap a[" << i << "][" << j << "]: ";
+			cin >> a[i][j];
+		}
+}
+
+int ViTriDuongDau(int a[], int n)
+{
+	for (int i = 0; i < n; i++)
+		if (a[i] > 0)
+			return i;
+	return -1;
+}
+
+int ViTriDuongNhoNhat(int a[], int n)
+{
+	int lc = ViTriDuongDau(a, n);
+	if (lc == -1)
+		return -1;
+	for (int i = 0; i < n; i++)
+		if (a[i] > 0 && a[i] < a[lc])
+			lc = i;
+	return lc;
+}
+
+int TimChuSo(int a[][500], int m, int n)
+{
+	int dem[10] = { 0,0,0,0,0,0,0,0,0,0 };
+	for(int i=0;i<m;i++)
+		for(int j=0;j<n;j++)
+		{
+			if (a[i][j] == 0)
+				dem[0]++;
+			int t = abs(a[i][j]);
+			while (t != 0)
+			{
+				int dv = t % 10;
+				dem[dv]++;
+				t /= 10;
+			}
+		}
+	return ViTriDuongNhoNhat(dem, 10);
 }
