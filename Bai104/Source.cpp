c8ca51878131f3dd3125c6cc1b831155f6e18f98
KevinNitroG <trannguyenthaibinh46@gmail.com>
2023-10-18 11:56:38 +0700

    104 Done

--- Bai104/Source.cpp
+++ Bai104/Source.cpp
@@ -1,8 +1,44 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
+void Nhap(float[][500], int &, int &);
+bool KtToanDuong(float[][500], int, int);
 
 int main()
 {
+	float arr[500][500];
+	int m;
+	int n;
 
+	Nhap(arr, m, n);
+	if (KtToanDuong(arr, m, n))
+		cout << "Ma tran toan duong";
+	else
+		cout << "Ma tran khong toan duong";
 	return 0;
+}
+
+void Nhap(float a[][500], int &m, int &n)
+{
+	cout << "Nhap so hang cua ma tran: ";
+	cin >> m;
+	cout << "Nhap so cot cua ma tran: ";
+	cin >> n;
+	for (int i = 0; i < m; i++)
+		for (int j = 0; j < n; j++)
+		{
+			cout << "Nhap a[" << i << "][" << j << "] : ";
+			cin >> a[i][j];
+		}
+}
+
+bool KtToanDuong(float a[][500], int m, int n)
+{
+	for (int i = 0; i < m; i++)
+		for (int j = 0; j < n; j++)
+		{
+			if (a[i][j] < 0)
+				return false;
+		}
+	return true;
 }
