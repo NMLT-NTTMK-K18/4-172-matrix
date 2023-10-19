f63ecea2e5c99865cc4a969c3264b23270a74b3e
khiemhoangtruongUIT <137704231+truonghoangkhiem@users.noreply.github.com>
2023-10-19 23:07:11 +0700

    Done 7,2

--- Bai002/Source.cpp
+++ Bai002/Source.cpp
@@ -1,8 +1,64 @@
 #include <iostream>
+#include <ctime>
+#include <iomanip>
+#include <cstdlib>
+#include <fstream>
+
 using namespace std;
+
+void TaoMaTran(float[][500], int&, int&);
+void Xuat(float[][500], int, int);
+void Xuat(string, float[][500], int, int);
 
 int main()
 {
-
+	float a[500][500];
+	int m, n;
+	TaoMaTran(a, m, n);
+	Xuat(a, m, n);
+	Xuat("data06.inp", a, m, n);
 	return 0;
+}
+
+void TaoMaTran(float a[][500], int& m, int& n)
+{
+	cout << "Nhap So Dong: ";
+	cin >> m;
+	cout << "Nhap So Cot: ";
+	cin >> n;
+	srand(time(NULL));
+	for (int i = 0; i < m; i++)
+		for (int j = 0; j < n; j++)
+			a[i][j] = -100.0 + (rand() / (RAND_MAX / (200.0)));
+}
+
+void Xuat(float a[][500], int m, int n)
+{
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cout << setw(8);
+			cout << setprecision(5);
+			cout << a[i][j];
+		}
+		cout << "\n";
+	}
+	
+}
+
+void Xuat(string filename, float a[][500], int m, int n)
+{
+	cout << "Xuat ma tran ra file: " << filename << "\n";
+	ofstream fo(filename);
+	fo << setw(8) << m;
+	fo << setw(8) << n;
+	fo << "\n";
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++)
+			fo << setw(8) << a[i][j];
+		fo << "\n";
+	}
+	cout << "Da xuat thanh cong!"<<endl;
 }
--- Bai007/Source.cpp
+++ Bai007/Source.cpp
@@ -1,8 +1,41 @@
 #include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <fstream>
+#include <iomanip>
+#include <string>
 using namespace std;
+
+void Nhap(float[][500], int, int);
+void Xuat(float[][500], int, int);
+
 
 int main()
 {
-
+	float a[500][500];
+	int m, n;
+	cin >> m >> n;
+	string fileName = "intdata02.inp";
+	Nhap(fileName, a, m, n);
+	Xuat(a, m, n);
+	
 	return 0;
+}
+
+void Nhap(string filename, float a[][500], int m, int n)
+{
+	ifstream fi(filename);
+	for (int i = 0; i < m; i++)
+		for (int j = 0; j < n; j++)
+			fi >> a[i][j];
+}
+
+void Xuat(float a[][500], int m, int n)
+{
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++)
+			cout << a[i][j] << " ";
+		cout << endl;
+	}
 }
