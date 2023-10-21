cd599b3c2181eb35b89a39b944243368a6828569
khiemhoangtruongUIT <137704231+truonghoangkhiem@users.noreply.github.com>
2023-10-22 00:02:53 +0700

    77->107 completed

--- Bai102/Source.cpp
+++ Bai102/Source.cpp
@@ -1,8 +1,42 @@
 #include <iostream>
 using namespace std;
 
+void Nhap(int[][500], int&, int&);
+int TonTaiLe(int[][500], int, int);
+
 int main()
 {
+	int a[500][500];
+	int m, n;
+	Nhap(a, m, n);
 
+	if (TonTaiLe(a, m, n))
+		cout << "Ma Tran ton tai gia tri le";
+	else
+		cout << "Ma Tran khong ton tai gia tri le";
+	return 0;
+}
+
+void Nhap(int a[][500], int& m, int& n)
+{
+	cout << "Nhap so dong: ";
+	cin >> m;
+	cout << "Nhap so cot: ";
+	cin >> n;
+	for (int i = 0; i < m; i++)
+		for (int j = 0; j < n; j++)
+		{
+			cout << "Nhap a[" << i << "][" << j << "]: ";
+			cin >> a[i][j];
+		}
+}
+
+int TonTaiLe(int a[][500], int m, int n)
+{
+	int flag = 0;
+	for (int i = 0; i < m; i++)
+		for (int j = 0; j < n; j++)
+			if (a[i][j] % 2 != 0)
+				return 1;
 	return 0;
 }
--- Bai107/Source.cpp
+++ Bai107/Source.cpp
@@ -1,8 +1,70 @@
 #include <iostream>
 using namespace std;
 
+void Nhap(float[][500], int&, int&);
+bool ktCotGiamDan(float [][500], int , int , int );
+bool ktDongGiamDan(float[][500], int, int, int);
+int ktGiam(float[][500], int, int);
+
 int main()
 {
+	float a[500][500];
+	int m, n;
+	Nhap(a, m, n);
 
+	if (ktGiam(a,m,n))
+		cout << "Ma Tran Giam Dan";
+	else
+		cout << "Ma Tran Khong Giam Dan";
 	return 0;
+}
+
+void Nhap(float a[][500], int& m, int& n)
+{
+	cout << "Nhap so dong: ";
+	cin >> m;
+	cout << "Nhap so cot: ";
+	cin >> n;
+	for (int i = 0; i < m; i++)
+		for (int j = 0; j < n; j++)
+		{
+			cout << "Nhap a[" << i << "][" << j << "]: ";
+			cin >> a[i][j];
+		}
+}
+
+bool ktCotGiamDan(float a[][500], int m, int n, int c)
+{
+	float lc = a[0][c];
+	for (int i = 1; i < m; i++)
+	{
+		if (a[i][c] >= lc)
+			return false;
+		lc = a[i][c];
+	}
+	return true;
+}
+
+bool ktDongGiamDan(float a[][500], int m, int n, int d)
+{
+	float lc = a[d][0];
+	for (int i = 1; i < n; i++)
+	{
+		if (a[d][i] >= lc)
+			return false;
+		lc = a[d][i];
+	}
+	return true;
+}
+
+int ktGiam(float a [][500], int m, int n)
+{
+	int flag = 1;
+	for(int i=0;i<m;i++)
+		for (int j = 0; j < n; j++)
+		{
+			if (!(ktDongGiamDan(a, m, n, i) && ktCotGiamDan(a, m, n, j)))
+				flag = 0;
+		}
+	return flag;
 }
