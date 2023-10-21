c5334c20dac76f3c0368e5395ab979a7fe438e04
Tuệ Dương Thông <23521734@gm.uit.edu.vn>
2023-10-21 08:45:14 +0700

    Bai130. Completed

--- Bai130/Source.cpp
+++ Bai130/Source.cpp
@@ -1,8 +1,74 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
+
+void Nhap(float[][500], int&, int&);
+void Xuat(float[][500], int, int);
+void DuongTang(float[][500], int, int);
+void HoanVi(float&, float&);
 
 int main()
 {
+	float a[500][500];
+	int m, n;
 
+	Nhap(a, m, n);
+	DuongTang(a, m, n);
+	Xuat(a, m, n);
 	return 0;
+}
+
+void Nhap(float a[][500], int& m, int& n)
+{
+	cout << "Nhap so hang: ";
+	cin >> m;
+	cout << "Nhap so cot: ";
+	cin >> n;
+	for (int i = 0; i < m; i++)
+		for (int j = 0; j < n; j++)
+		{
+			cout << "Nhap a[" << i << "][" << j << "]: ";
+			cin >> a[i][j];
+		}
+}
+
+void Xuat(float a[][500], int m, int n)
+{
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++)
+			cout << fixed << setprecision(2) << setw(10) << a[i][j];
+		cout << endl;
+	}
+}
+
+void DuongTang(float a[][500], int m, int n)
+{
+	float b[500];
+	int k;
+	int i, j;
+
+	k = 0;
+	for (i = 0; i < m; i++)
+		for (j = 0; j < n; j++)
+			if (a[i][j] > 0)
+				b[k++] = a[i][j];
+
+	for (i = 0; i <= k - 2; i++)
+		for (j = i + 1; j <= k - 1; j++)
+			if (b[i] > b[j])
+				HoanVi(b[i], b[j]);
+
+	k = 0;
+	for (i = 0; i < m; i++)
+		for (j = 0; j < n; j++)
+			if (a[i][j] > 0)
+				a[i][j] = b[k++];
+}
+
+void HoanVi(float& t, float& k)
+{
+	float temp = t;
+	t = k;
+	k = temp;
 }
