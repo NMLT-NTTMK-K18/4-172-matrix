dc86ce55f4d6c8f37f147ad4dd7dce61e5ad8b49
Tuệ Dương Thông <23521734@gm.uit.edu.vn>
2023-10-23 20:19:37 +0700

    Bai155. Completed

--- Bai155/Source.cpp
+++ Bai155/Source.cpp
@@ -1,8 +1,57 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
+
+void Nhap(float[][500], int&, int&);
+void Xuat(float[][500], int, int);
+void DichXuongCot(float[][500], int, int, int);
+void DichXuong(float[][500], int, int);
 
 int main()
 {
+	float a[500][500];
+	int m, n;
 
+	Nhap(a, m, n);
+	DichXuong(a, m, n);
+	Xuat(a, m, n);
 	return 0;
+}
+
+void Nhap(float a[][500], int& m, int& n)
+{
+	cout << "Nhap so hang: ";
+	cin >> m;
+	cout << "Nhap so cot: ";
+	cin >> n;
+	for (int i = 0; i < m; i++)
+		for (int j = 0; j < n; j++)
+		{
+			cout << "Nhap a[" << i << "][" << j << "]: ";
+			cin >> a[i][j];
+		}
+}
+
+void Xuat(float a[][500], int m, int n)
+{
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++)
+			cout << setw(10) << fixed << setprecision(2) << a[i][j];
+		cout << endl;
+	}
+}
+
+void DichXuongCot(float a[][500], int m, int n, int c)
+{
+	float temp = a[m - 1][c];
+	for (int i = m - 1; i >= 1; i--)
+		a[i][c] = a[i - 1][c];
+	a[0][c] = temp;
+}
+
+void DichXuong(float a[][500], int m, int n)
+{
+	for (int j = 0; j < n; j++)
+		DichXuongCot(a, m, n, j);
 }
